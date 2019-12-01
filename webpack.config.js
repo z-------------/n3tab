3e5f1745c4c49df3b5ec588eed57c3739e0b9c3b
Zack Guard <zacharyguard@gmail.com>
2019-12-01 22:24:57 +0800

    Configure webpack for an options page

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,24 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
-    entry: ["./src/main.ts", "./src/index.pug", "./src/style.sass"],
+    entry: {
+        "main": ["./src/main.ts", "./src/index.pug", "./src/style.sass"],
+        "options": ["./src/options/index.pug", "./src/options/style.sass"]
+    },
     devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
+            chunks: ["main"],
+            filename: "./dist/main/index.html",
             template: "./src/index.pug"
         }),
+        new HtmlWebpackPlugin({
+            chunks: ["options"],
+            filename: "./dist/options/index.html",
+            template: "./src/options/index.pug"
+        }),
         new MiniCssExtractPlugin({
-            filename: "[name].css",
+            filename: "./dist/[name]/style.css",
             chunkFilename: "[id].css"
         })
     ],
@@ -27,16 +37,20 @@ module.exports = {
         }, {
             test: /\.s[ac]ss$/,
             include: path.join(__dirname, "src"),
-            use: [{
-                loader: MiniCssExtractPlugin.loader
-            }, "css-loader", "sass-loader"]
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader
+                },
+                "css-loader",
+                "sass-loader",
+            ]
         }]
     },
     resolve: {
         extensions: [".ts", ".js"]
     },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "dist/[name]/bundle.js",
+        path: __dirname
     }
 };
