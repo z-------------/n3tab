3340777e27d453f6017896b9b612fc34ef1f6c62
Zack Guard <zacharyguard@gmail.com>
2019-12-02 20:50:03 +0800

    Update HK regional WX icons

--- src/regionalWeatherInfo/hkRegionalWeatherInfo.ts
+++ src/regionalWeatherInfo/hkRegionalWeatherInfo.ts
@@ -2,9 +2,11 @@ import get from "../../lib/get";
 import { parseXML } from "../../lib/parse";
 import IRegionalWeatherInfo, { ASSETS_PATH } from "./iRegionalWeatherInfo";
 
+const FILE_EXT = "svg";
+
 const iconMap = new Map([
-    ["yellow fire danger warning", "warning_firey"],
-    ["red fire danger warning", "warning_firer"],
+    ["yellow fire danger warning", "warning_firey_no_text"],
+    ["red fire danger warning", "warning_firer_no_text"],
     ["standby signal, no. 1", "No._01_Standby_Signal"],
     ["strong wind signal, no. 3", "No._03_Strong_Wind_Signal"],
     ["no. 8 northeast gale or storm signal", "No._8_Northeast_Gale_or_Storm_Signal"],
@@ -13,17 +15,17 @@ const iconMap = new Map([
     ["no. 8 southwest gale or storm signal", "No._8_Southwest_Gale_or_Storm_Signal"],
     ["increasing gale or storm signal, no. 9", "No._09_Increasing_Gale_or_Storm_Signal"],
     ["hurricane signal, no. 10", "No._10_Hurricane_Signal"],
-    ["amber rainstorm warning signal", "Rainstorm_Amber"],
-    ["red rainstorm warning signal", "Rainstorm_Red"],
-    ["black rainstorm warning signal", "Rainstorm_Black"],
-    ["thunderstorm warning", "Thunderstorm_Warning"],
+    ["amber rainstorm warning signal", "Rainstorm_Amber_no_text"],
+    ["red rainstorm warning signal", "Rainstorm_Red_no_text"],
+    ["black rainstorm warning signal", "Rainstorm_Black_no_text"],
+    ["thunderstorm warning", "Thunderstorm_Warning_no_text"],
     ["special announcement on flooding in northern new territories", "warning_ntfl"],
     ["landslip warning", "Landslip"],
-    ["strong monsoon signal", "warning_sms"],
-    ["frost warning", "warning_frost"],
-    ["cold weather warning", "Cold_Weather_Warning"],
-    ["very hot weather warning", "Very_Hot_Weather_Warning"],
-    ["tsunami warning", "warning_tsunami_warn"],
+    ["strong monsoon signal", "warning_sms_no_text"],
+    ["frost warning", "warning_frost_no_text"],
+    ["cold weather warning", "Cold_Weather_Warning_no_text"],
+    ["very hot weather warning", "Very_Hot_Weather_Warning_no_text"],
+    ["tsunami warning", "warning_tsunami_no_text"],
 ]);
 
 export default class HKRegionalWeatherInfo implements IRegionalWeatherInfo {
@@ -43,7 +45,7 @@ export default class HKRegionalWeatherInfo implements IRegionalWeatherInfo {
         const warningStr = titleStr.substring(0, titleStr.indexOf(" issued"));
         if (!warningStr) return null;
         const warningKey = warningStr.toLowerCase();
-        const icon = `${ASSETS_PATH}/hk/${iconMap.get(warningKey)}.png`;
+        const icon = `${ASSETS_PATH}/hk/${iconMap.get(warningKey)}.${FILE_EXT}`;
         return {
             summary: warningStr,
             icon
