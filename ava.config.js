0a9cded63564ec0d9d08ea4d185877179e334eb4
Zack Guard <zacharyguard@gmail.com>
2019-12-05 23:13:43 +0800

    I think I've got it...

--- ava.config.js
+++ ava.config.js
@@ -3,13 +3,8 @@ const fs = require("fs");
 export default () => {
     console.log(process.cwd());
     console.log(fs.readdirSync("test"));
-	if (process.env.GITHUB_ACTION) {
-		return {
-			require: ["/home/runner/work/n3tab/n3tab/test/_setup-browser-env.js"]
-		};
-	}
-
 	return {
-		require: ["./test/_setup-browser-env.js"]
+        helpers: ["./test/setup-browser-env.js"],
+		require: ["./test/setup-browser-env.js"]
 	};
 };
