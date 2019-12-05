5d2f3de9b951c33a154b62524adedd0ad1da804b
Zack Guard <zacharyguard@gmail.com>
2019-12-05 23:07:50 +0800

    Try to debug Actions...

--- ava.config.js
+++ ava.config.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+
 export default () => {
+    console.log(process.cwd());
+    console.log(fs.readdirSync("test"));
 	if (process.env.GITHUB_ACTION) {
 		return {
 			require: ["/home/runner/work/n3tab/n3tab/test/_setup-browser-env.js"]
