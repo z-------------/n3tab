51d75056c8661059dea1eb46862900b87c4c42f2
Zack Guard <zacharyguard@gmail.com>
2019-12-05 23:03:26 +0800

    Try harder

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,11 @@
-export default {
-    require: [
-        "./test/_setup-browser-env"
-    ]
+export default () => {
+	if (process.env.GITHUB_ACTION) {
+		return {
+			require: ["/home/runner/work/n3tab/n3tab/test/_setup-browser-env.js"]
+		};
+	}
+
+	return {
+		require: ["./test/_setup-browser-env.js"]
+	};
 };
